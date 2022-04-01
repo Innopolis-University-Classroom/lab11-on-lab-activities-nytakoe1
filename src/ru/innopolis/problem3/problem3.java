0f00c199bae2d5805d57848ffba5b824cb32393c
nytakoe1 <88328194+nytakoe1@users.noreply.github.com>
2022-04-01 17:18:50 +0300

    Update problem3.java

--- src/ru/innopolis/problem3/problem3.java
+++ src/ru/innopolis/problem3/problem3.java
@@ -1,2 +1,51 @@
-// your solution here
-//fix all the errors including package errors
+public class Main {
+
+    public static void main(String[] args) {
+        Media media = new Media();
+        Audio audio= new Audio();
+        MP3Adapter audio2= new MP3Adapter(audio);
+        System.out.println("Media:");
+        media.playMP4();
+        System.out.println("Audio:");
+        audio.playMP3();
+        System.out.println("Audio2:");
+        audio2.playMP4();
+    }
+}
+
+interface MediaPlayer{
+    public void playMP4();
+}
+
+class Media implements MediaPlayer{
+
+    @Override
+    public void playMP4() {
+        System.out.println("currently in use MB4");
+    }
+}
+
+interface AudioPlayer{
+    public void playMP3();
+}
+
+class Audio implements  AudioPlayer{
+
+    @Override
+    public void playMP3() {
+        System.out.println( "currently in use MB3");
+    }
+}
+
+class MP3Adapter implements  MediaPlayer{
+    AudioPlayer audioPlayer;
+
+    public MP3Adapter(AudioPlayer au){
+        this.audioPlayer=au;
+    }
+    @Override
+    public void playMP4() {
+        audioPlayer.playMP3();
+
+    }
+}
