e2384bc91e3dc5c7db797c4f7fe4ad48eca634f6
nytakoe1 <88328194+nytakoe1@users.noreply.github.com>
2022-04-01 16:58:46 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,51 @@
-// your solution here
-//fix all the errors including package errors
+public class Main {
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        Android android= new Android();
+        Android android2= new Adapter(iphone);
+
+        iphone.chargeByLightning();
+        android.chargeByUSB();
+        android2.chargeByUSB();
+
+    }
+}
+
+interface LightningPort{
+    public void chargeByLightning();
+}
+
+class Iphone implements LightningPort{
+
+    @Override
+    public void chargeByLightning() {
+        System.out.println("your Phone is charging by Lightning");
+    }
+}
+
+interface USBPort{
+    public void chargeByUSB();
+}
+
+class Android implements USBPort{
+
+    @Override
+    public void chargeByUSB() {
+        System.out.println("your Phone is charging by USB");
+
+    }
+}
+
+class Adapter extends Android implements USBPort{
+    LightningPort lightningPort;
+
+    public Adapter(LightningPort lp){
+        this.lightningPort=lp;
+    }
+
+    @Override
+    public void chargeByUSB() {
+        lightningPort.chargeByLightning();
+    }
+}
